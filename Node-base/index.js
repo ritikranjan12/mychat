5359bb83e2e34506278ee64914baa3ba098f7c3f
Ritik <ritik123453@gmail.com>
2022-02-19 15:17:40 +0530

    Initial commit

---
+++ Node-base/index.js
@@ -0,0 +1,20 @@
+const io = require("socket.io")(8009)
+
+const users = {};
+
+io.on('connection',socket=>{
+    
+    socket.on('new-user-joined',name =>{
+        users[socket.id]=name;
+        socket.broadcast.emit('user-joined',name)
+    });
+
+    socket.on('send',message =>{
+        socket.broadcast.emit('recieve',{message:message,name:users[socket.id]})
+    });
+
+    socket.on('disconnect',message =>{
+        socket.broadcast.emit('left',users[socket.id])
+        delete users[socket.id]
+    });
+});
